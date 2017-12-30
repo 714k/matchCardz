9de78668e705cb0e4688b2d26b2ceca0375221a6
victorZamudio <victor.zamudio@hotmail.com>
2017-12-29 22:45:06 -0600

    FEATURE | Main screen - Added Main screen initial as app entry point - Added react-native-vector-icons module

---
+++ app/screens/MainScreen/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  btn: {
+    marginTop: 20,
+  },
+  logoA: {
+    textAlign: 'center',
+    color: '#3e586b',
+    lineHeight: 30
+  },
+  logoB: {
+    textAlign: 'center',
+    color: '#3e586b',
+    fontSize: 48,
+    lineHeight: 18,
+  }
+});
